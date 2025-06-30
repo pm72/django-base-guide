efd13dca7309ad6c9cc4648e055cd0bcf1222a28
pm72 <46081859+pm72@users.noreply.github.com>
2025-06-30 21:44:54 +0400

    pm72

--- shop/main/views.py
+++ shop/main/views.py
@@ -10,7 +10,7 @@ def product_list(request, category_slug=None):
   category = None
 
   if category_slug:
-    category = get_object_or_404(Category, category_slug=category_slug)
+    category = get_object_or_404(Category, slug=category_slug)
     products = products.filter(category=category)
   
   context = {
@@ -20,3 +20,15 @@ def product_list(request, category_slug=None):
   }
 
   return render(request, 'main/product/list.html', context)
+
+
+def product_detail(request, pk, slug):
+  product = get_object_or_404(Product, pk=pk, slug=slug, available=True)
+  related_products = Product.objects.filter(category=product.category,
+                                            available=True).exclude(pk=product.pk)[:4]
+  context = {
+    'product': product,
+    'related_products': related_products,
+  }
+
+  return render(request, 'main/product/detail.html', context)
