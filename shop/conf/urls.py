b4f943399beb71cd721eced8d6318435f50e55b0
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-02 20:57:39 +0400

    pm72

--- shop/conf/urls.py
+++ shop/conf/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
   path('', include('main.urls', namespace='main')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
