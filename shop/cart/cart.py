ce34f04fe646bfe8bc71d3ee37b416d6fa39b3b4
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-05 14:55:23 +0400

    pm72: Cart functional

--- shop/cart/cart.py
+++ shop/cart/cart.py
@@ -56,3 +56,18 @@ class Cart:
       item['total_price'] = item['price'] * item['quantity']
 
       yield item
+  
+
+  def __len__(self):
+    return sum(item['quantity'] for item in self.cart.values())
+  
+
+  def get_total_price(self):
+    return sum(Decimal(item['price']) * item['quantity']
+               for item in self.cart.values())
+  
+
+  def clear(self):
+    del self.session[settings.CAR_SESSION_ID]
+
+    self.save()
