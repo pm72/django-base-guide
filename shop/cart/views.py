31921af82397afd57baf6d96b3db4ae8b0f8f3d4
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-05 23:57:05 +0400

    pm72: Cart class forms and views

--- shop/cart/views.py
+++ shop/cart/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.models import Product
+from . cart import Cart
+from . forms import CartAddProductForm
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+
+@require_POST
+def cart_add()
