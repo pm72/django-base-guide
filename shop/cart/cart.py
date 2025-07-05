e41a2014d0f1b9e0024fedda671f485c03e1a574
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-05 14:00:16 +0400

    pm72: cart functions

--- shop/cart/cart.py
+++ shop/cart/cart.py
@@ -1,13 +1,11 @@
 from django.conf import settings
 from main.models import Product
+from decimal import Decimal
 
 
 class Cart:
   def __init__(self, request):
-    # სესიის ინიციალიზაცია
     self.session = request.session
-
-    # მივიღოთ მომხმარებლის სესია (კალათა)
     cart = self.session.get(settings.CART_SESSION_ID)
 
     if not cart:
@@ -16,4 +14,45 @@ class Cart:
     self.cart = cart
   
 
+  def add(self, product, quantity=1, override_quantity=False):
+    product_id = str(product.id)
+
+    if product_id not in self.cart:
+      self.cart[product_id] = {
+        'quantity': 0,
+        'price': str(product.price)
+      }
+
+    if override_quantity:
+      self.cart[product_id]['quantity'] = quantity
+    else:
+      self.cart[product_id]['quantity'] += quantity
+    
+    self.save()
   
+
+  def remove(self, product):
+    product_id = product.id
+
+    if product_id in self.cart:
+      del self.cart[product_id]
+      self.save()
+  
+  
+  def save(self):
+    self.session.modified = True
+  
+
+  def __iter__(self):
+    product_ids = self.cart.keys()
+    products = Product.objects.filter(id__in=product_ids)
+    cart = self.cart.copy()
+
+    for product in products:
+      cart[str(product.id)]['product'] = product
+    
+    for item in cart.values():
+      item['price'] = Decimal(item['price'])
+      item['total_price'] = item['price'] * item['quantity']
+
+      yield item
