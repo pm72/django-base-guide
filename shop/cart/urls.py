244bd09f63891d0b46a52d70a1a6c06104f8b470
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-07 00:22:28 +0400

    pm72: Cart – urls and templates

--- shop/cart/urls.py
+++ shop/cart/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'cart'
 
-urlspattern = [
+urlpatterns = [
   path('', views.cart_detail, name='cart-detail'),
   path('add/<int:product_id>', views.cart_add, name='cart-add'),
   path('remove/<int:product_id>', views.cart_remove, name='cart-remove'),
--- shop/conf/urls.py
+++ shop/conf/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 urlpatterns = [
 	path('admin/', admin.site.urls),
   path('', include('main.urls', namespace='main')),
-  path('cart/', include('cart.urls')),
+  path('cart/', include('cart.urls', namespace='cart')),
 ]
 
 if settings.DEBUG:
