aa5211e22e90e3261d692a88c61ea7f248d0b859
pm72 <46081859+pm72@users.noreply.github.com>
2025-07-12 21:08:42 +0400

    pm72

--- shop/main/admin.py
+++ shop/main/admin.py
@@ -6,7 +6,7 @@ from . models import Category, Product
 class CategoryAdmin(admin.ModelAdmin):
   list_display = ('name', 'slug')
   prepopulated_fields = {'slug': ('name',)}
-
+  
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
